671bf22ca9b771a714009b02bb5fb65303761661
jamesaspinall <james.aspinall@agridigital.io>
2021-03-26 18:50:44 +1100

    Retry infinty loop

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,51 +9,59 @@ import {
 const IndexPage = ({ location }) => {
   const dispatch = useContext(GlobalDispatchContext);
   const contextState = useContext(GlobalStateContext);
-  const token = location?.hash.split("=")[1];
-
-  console.log("INDEX TOKEN", token);
-  console.log("Index ContextState", contextState);
-
-  if (token) {
-    dispatch({ type: "SET_INVITE_TOKEN", token: token });
-  }
 
   useEffect(() => {
+    const token = location?.hash.split("=")[1];
     dispatch({ type: "SET_INVITE_TOKEN", token: token });
-  }, [token]);
+    console.log("INDEX TOKEN", token);
+  }, []);
 
-  // const auth = new GoTrue({
-  //   APIUrl: "https://dwguitars.netlify.app/.netlify/identity",
-  //   audience: "",
-  //   setCookie: false,
-  // });
+  console.log("Index ContextState", contextState);
 
-  // const [state, setState] = useState("");
+  const auth = new GoTrue({
+    APIUrl: "https://dwguitars.netlify.app/.netlify/identity",
+    audience: "",
+    setCookie: false,
+  });
 
-  // const handleChange = (evt) => {
-  //   const value = evt.target.value;
-  //   setState({
-  //     ...state,
-  //     [evt.target.name]: value,
-  //   });
-  //   console.log(state);
-  // };
+  const [login, setLogin] = useState("");
 
-  // const handleLogin = () => {
-  //   const { email, password } = state;
-  //   auth
-  //     .login(email, password, true)
-  //     .then((response) => {
-  //       console.log(response);
-  //       console.log(JSON.stringify(response));
-  //     })
-  //     .catch((error) => console.error(error));
-  // };
+  const handleChange = (evt) => {
+    const value = evt.target.value;
+    setLogin({
+      ...login,
+      [evt.target.name]: value,
+    });
+    console.log(login);
+  };
+
+  const handleLogin = () => {
+    const { email, password } = login;
+    auth
+      .login(email, password, true)
+      .then((response) => {
+        console.log("LOGIN RES", response);
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <main>
       <title>Home Page</title>
       <h1>Welcome To DW Guitars Academy</h1>
+      <label htmlFor="email">
+        Email
+        <input name="email" type="email" onChange={(e) => handleChange(e)} />
+      </label>
+      <label htmlFor="email">
+        Password
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => handleChange(e)}
+        />
+      </label>
+      <button onClick={handleLogin}>login</button>
       <Link to="/setpassword">Set Password Page</Link>
     </main>
   );
