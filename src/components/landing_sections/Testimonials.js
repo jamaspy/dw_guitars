55de62dcc97929088258099e73b5b3a98c315dea
jamesaspinall <james.aspinall@agridigital.io>
2021-03-28 09:43:49 +1100

    Structure Testimonials

--- src/components/landing_sections/Testimonials.js
+++ src/components/landing_sections/Testimonials.js
@@ -1,7 +1,38 @@
 import React from "react";
-
+import * as testimonialStyles from "../../scss/testimonials.module.scss";
+import { ImQuotesRight } from "react-icons/im";
 const Testimonials = () => {
-  return <div>TESTIMONIALS</div>;
+  return (
+    <main className={testimonialStyles.container}>
+      <div className={testimonialStyles.testimonial_main_container}>
+        <div className={testimonialStyles.testimonial_icon}>
+          <ImQuotesRight />
+        </div>
+        <div className={testimonialStyles.testimonial_quote}>
+          Truly Awesome Content. I highly recommend!
+        </div>
+        <div className={testimonialStyles.testimonial_avatar}></div>
+      </div>
+      <div className={testimonialStyles.testimonial_main_container}>
+        <div className={testimonialStyles.testimonial_icon}>
+          <ImQuotesRight />
+        </div>
+        <div className={testimonialStyles.testimonial_quote}>
+          Truly Awesome Content. I highly recommend!
+        </div>
+        <div className={testimonialStyles.testimonial_avatar}></div>
+      </div>
+      <div className={testimonialStyles.testimonial_main_container}>
+        <div className={testimonialStyles.testimonial_icon}>
+          <ImQuotesRight />
+        </div>
+        <div className={testimonialStyles.testimonial_quote}>
+          Truly Awesome Content. I highly recommend!
+        </div>
+        <div className={testimonialStyles.testimonial_avatar}></div>
+      </div>
+    </main>
+  );
 };
 
 export default Testimonials;
